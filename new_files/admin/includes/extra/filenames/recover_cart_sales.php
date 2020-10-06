4281e9a28f1b9682d73d4d5a1fc6908c55922ba5
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-06 10:40:59 +0200

    refactor: remove unnecessary comment

--- new_files/admin/includes/extra/filenames/recover_cart_sales.php
+++ new_files/admin/includes/extra/filenames/recover_cart_sales.php
@@ -1,7 +1,5 @@
 <?php
-// BOF Offener Warenkorb Plus
 define('FILENAME_RECOVER_CART_SALES', 'recover_cart_sales.php');
 define('FILENAME_STATS_RECOVER_CART_SALES', 'stats_recover_cart_sales.php');
 define('FILENAME_CATALOG_PRODUCT_INFO', 'product_info.php');
 define('FILENAME_CATALOG_LOGIN', 'login.php');
-// EOF Offener Warenkorb Plus
