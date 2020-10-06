276cfcbc0e33fa1ed9545e30e8ba16c6ed879e7e
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-06 13:35:13 +0200

    docs: update module description

--- new_files/lang/german/modules/system/McmRecoverCartSales.php
+++ new_files/lang/german/modules/system/McmRecoverCartSales.php
@@ -3,7 +3,7 @@ $moduleType = 'MODULE';
 $moduleName = 'MCM_RECOVER_CART_SALES';
 $prefix = $moduleType . '_' . $moduleName  . '_';
 
-define($prefix . 'TITLE', 'DE Title von ' . $moduleType . '_' . $moduleName);
-define($prefix . 'LONG_DESCRIPTION', 'DE Long Description von ' . $moduleType . '_' . $moduleName);
+define($prefix . 'TITLE', 'Offene Warenkörbe Plus © <a href="https://github.com/ModifiedCommunityModules/recover-cart-sales" target="_blank" style="color: #b0377e; font-weight: bold;">by many Authors</a>');
+define($prefix . 'LONG_DESCRIPTION', 'Beschreibung von Offene Warenkörbe Plus<br><br>Für dieses Modul kann Support freundlich im modified Forum erfragt werden. Ein Anspruch auf Support besteht jedoch nicht. Alternativ kann ein Dienstleister kostenpflichtig beauftragt werden.');
 define($prefix . 'STATUS_TITLE', 'DE Status Title von ' . $moduleType . '_' . $moduleName);
 define($prefix . 'STATUS_DESC', 'DE Status Description von ' . $$moduleType . '_' . $moduleName);
