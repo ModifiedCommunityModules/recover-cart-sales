91ce9af1a3809f8af226aea7e1855c926aa36677
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-06 12:08:10 +0200

    feat: add System Module

---
+++ new_files/admin/includes/modules/system/McmRecoverCartSales.php
@@ -0,0 +1,33 @@
+<?php
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// restore_error_handler();
+// restore_exception_handler();
+
+defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
+
+use RobinTheHood\ModifiedStdModule\Classes\StdModule;
+require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/autoload.php';
+
+class McmRecoverCartSales extends StdModule
+{
+    public function __construct()
+    {
+        $this->init('MODULE_MCM_RECOVER_CART_SALES');
+    }
+       
+    public function display()
+    {
+        return $this->displaySaveButton();
+    }
+
+    public function install()
+    {
+        parent::install();
+    }
+
+    public function remove()
+    {
+        parent::remove();
+    }
+}
