64e333b0cbecc74a17b18142756782b07a8e4199
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-06 14:03:26 +0200

    refactor: rename file

---
+++ new_files/admin/includes/extra/filenames/mcm_recover_cart_sales.php
@@ -0,0 +1,5 @@
+<?php
+define('FILENAME_MCM_RECOVER_CART_SALES', 'mcm_recover_cart_sales.php');
+define('FILENAME_MCM_RECOVER_CART_SALES_STATS', 'mcm_recover_cart_sales_stats.php');
+define('FILENAME_CATALOG_PRODUCT_INFO', 'product_info.php');
+define('FILENAME_CATALOG_LOGIN', 'login.php');
--- new_files/admin/includes/extra/filenames/recover_cart_sales.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('FILENAME_RECOVER_CART_SALES', 'recover_cart_sales.php');
-define('FILENAME_STATS_RECOVER_CART_SALES', 'stats_recover_cart_sales.php');
-define('FILENAME_CATALOG_PRODUCT_INFO', 'product_info.php');
-define('FILENAME_CATALOG_LOGIN', 'login.php');
