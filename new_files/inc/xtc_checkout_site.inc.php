4782bcd7d2c309b48c8ea919aeb25c289f06b968
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-08 17:51:42 +0200

    refactor: improve coding style, use PSR-12
    https://www.php-fig.org/psr/psr-12/

--- new_files/inc/xtc_checkout_site.inc.php
+++ new_files/inc/xtc_checkout_site.inc.php
@@ -10,21 +10,31 @@ Copyright (c) 2006 Estelco
 Released under the GNU General Public License
 ---------------------------------------------------------------------------------------*/
 
-function xtc_checkout_site($site) {
-    if (!$_SESSION['customer_id'] || ($site != 'cart' && $site != 'shipping' && $site != 'payment' && $site != 'confirm')) {
+function xtc_checkout_site($site)
+{
+    if (!$_SESSION['customer_id']) {
         return false;
     }
-    $checkQuery = xtc_db_query("SELECT checkout_site FROM " . TABLE_CUSTOMERS_BASKET . " WHERE customers_id=" . $_SESSION['customer_id']);
-    $checkResult = xtc_db_fetch_array($checkQuery);
-    compareSite($site, $checkResult['checkout_site']);
+
+    if ($site != 'cart' && $site != 'shipping' && $site != 'payment' && $site != 'confirm') {
+        return false;
+    }
+
+    $query = xtc_db_query("SELECT checkout_site FROM " . TABLE_CUSTOMERS_BASKET . " WHERE customers_id = " . $_SESSION['customer_id']);
+    $result = xtc_db_fetch_array($query);
+    compareSite($site, $result['checkout_site']);
 }
 
-function compareSite($currentSite, $oldSite) {
-    $cart = 1;
-    $shipping = 2;
-    $payment = 3;
-    $confirm = 4;
-    if ($$currentSite >= $$oldSite) {
-        xtc_db_query("UPDATE " . TABLE_CUSTOMERS_BASKET . " SET checkout_site='" . xtc_db_input($currentSite) . "', language='" . xtc_db_input($_SESSION['language']) . "' WHERE customers_id=" . (int)$_SESSION['customer_id']);
+function compareSite($currentSite, $oldSite)
+{
+    $sorting = [
+        'cart' => 1,
+        'shipping' => 2,
+        'payment' => 3,
+        'confirm' => 4
+    ];
+
+    if ($sorting[$currentSite] >= $sorting[$oldSite]) {
+        xtc_db_query("UPDATE " . TABLE_CUSTOMERS_BASKET . " SET checkout_site = '" . xtc_db_input($currentSite) . "', language = '" . xtc_db_input($_SESSION['language']) . "' WHERE customers_id = " . (int) $_SESSION['customer_id']);
     }
 }
