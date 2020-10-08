5b8a8f812c60d9f36a80d34b3ca728969065ee83
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-08 17:51:09 +0200

    refactor: improve coding style, use PSR-12
    https://www.php-fig.org/psr/psr-12/

--- new_files/includes/extra/application_top/application_top_begin/mcm_recover_cart_sales.php
+++ new_files/includes/extra/application_top/application_top_begin/mcm_recover_cart_sales.php
@@ -1,6 +1,6 @@
 <?php
 require_once DIR_FS_INC . 'xtc_checkout_site.inc.php';
 
-if(strpos($_SERVER['REQUEST_URI'], 'login') !== false && isset($_SESSION['customer_id'])) {
+if (strpos($_SERVER['REQUEST_URI'], 'login') !== false && isset($_SESSION['customer_id'])) {
     xtc_checkout_site('cart');
 }
