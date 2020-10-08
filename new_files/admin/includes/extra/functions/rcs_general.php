97ed1d062b4fbfe1b74cda9f76f3d518f9b69182
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-08 18:19:29 +0200

    refactor: remove comments

--- new_files/admin/includes/extra/functions/rcs_general.php
+++ new_files/admin/includes/extra/functions/rcs_general.php
@@ -1,7 +1,7 @@
 <?php
 
-// BOF Offener Warenkorb Plus
-function xtc_get_products_special_price_ow($product_id, $customer_id, $qty = 1) {
+function xtc_get_products_special_price_ow($product_id, $customer_id, $qty = 1)
+{
     $customer_group_query = xtc_db_query("select customers_status from " . TABLE_CUSTOMERS . " where customers_id = '" . $customer_id . "'");
     $customer_group = xtc_db_fetch_array($customer_group_query);
     $personal_query = xtc_db_query("SELECT personal_offer FROM " . TABLE_PERSONAL_OFFERS_BY . $customer_group['customers_status'] . " WHERE products_id=" . (int)$product_id . " AND quantity<=" . (int)$qty . " ORDER BY quantity DESC LIMIT 1");
@@ -13,4 +13,3 @@ function xtc_get_products_special_price_ow($product_id, $customer_id, $qty = 1) 
     $product = xtc_db_fetch_array($product_query);
     return $product['specials_new_products_price'];
 }
-// EOF Offener Warenkorb Plus
