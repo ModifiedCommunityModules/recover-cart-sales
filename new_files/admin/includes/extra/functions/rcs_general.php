06b779cb1a05fbd77df0f6cd818aff2242e37062
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-09 12:38:22 +0200

    refactor: improve coding style, use PSR-12
    https://www.php-fig.org/psr/psr-12/

--- new_files/admin/includes/extra/functions/rcs_general.php
+++ new_files/admin/includes/extra/functions/rcs_general.php
@@ -1,15 +1,17 @@
 <?php
 
-function xtc_get_products_special_price_ow($product_id, $customer_id, $qty = 1)
+function xtc_get_products_special_price_ow($productId, $customerId, $qty = 1)
 {
-    $customer_group_query = xtc_db_query("select customers_status from " . TABLE_CUSTOMERS . " where customers_id = '" . $customer_id . "'");
-    $customer_group = xtc_db_fetch_array($customer_group_query);
-    $personal_query = xtc_db_query("SELECT personal_offer FROM " . TABLE_PERSONAL_OFFERS_BY . $customer_group['customers_status'] . " WHERE products_id=" . (int)$product_id . " AND quantity<=" . (int)$qty . " ORDER BY quantity DESC LIMIT 1");
-    if (xtc_db_num_rows($personal_query)) {
-        $personal = xtc_db_fetch_array($personal_query);
+    $customerGroupQuery = xtc_db_query("SELECT customers_status FROM " . TABLE_CUSTOMERS . " WHERE customers_id = '" . $customerId . "'");
+    $customerGroup = xtc_db_fetch_array($customerGroupQuery);
+    $personalQuery = xtc_db_query("SELECT personal_offer FROM " . TABLE_PERSONAL_OFFERS_BY . $customerGroup['customers_status'] . " WHERE products_id = " . (int) $productId . " AND quantity <= " . (int) $qty . " ORDER BY quantity DESC LIMIT 1");
+    
+    if (xtc_db_num_rows($personalQuery)) {
+        $personal = xtc_db_fetch_array($personalQuery);
         return $personal['personal_offer'];
     }
-    $product_query = xtc_db_query("select specials_new_products_price from " . TABLE_SPECIALS . " where products_id = '" . (int)$product_id . "' and status");
-    $product = xtc_db_fetch_array($product_query);
+
+    $productQuery = xtc_db_query("SELECT specials_new_products_price FROM " . TABLE_SPECIALS . " WHERE products_id = '" . (int) $productId . "' AND status");
+    $product = xtc_db_fetch_array($productQuery);
     return $product['specials_new_products_price'];
 }
