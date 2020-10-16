30cf767d6247bb9898684b40a15f969f6f5d5fc6
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-16 13:02:09 +0200

    fix: change german title and description of option status

--- new_files/lang/german/modules/system/McmRecoverCartSales.php
+++ new_files/lang/german/modules/system/McmRecoverCartSales.php
@@ -5,5 +5,5 @@ $prefix = $moduleType . '_' . $moduleName  . '_';
 
 define($prefix . 'TITLE', 'Offene Warenkörbe Plus © <a href="https://github.com/ModifiedCommunityModules/recover-cart-sales" target="_blank" style="color: #b0377e; font-weight: bold;">by various Authors</a>');
 define($prefix . 'LONG_DESCRIPTION', 'Beschreibung von Offene Warenkörbe Plus<br><br>Für dieses Modul kann Support freundlich im modified Forum erfragt werden. Ein Anspruch auf Support besteht jedoch nicht. Alternativ kann ein Dienstleister kostenpflichtig beauftragt werden.');
-define($prefix . 'STATUS_TITLE', 'DE Status Title von ' . $moduleType . '_' . $moduleName);
-define($prefix . 'STATUS_DESC', 'DE Status Description von ' . $$moduleType . '_' . $moduleName);
+define($prefix . 'STATUS_TITLE', 'Status');
+define($prefix . 'STATUS_DESC', 'Modul Offene Warenkörbe Plus aktivieren?');
